b93e83890f1f08d785aea9bfcba15483033d3fdf
Renan Cammarosano <renancamm@gmail.com>
2021-08-18 17:05:58 +0200

    improves formatting output

--- decode.py
+++ decode.py
@@ -5,19 +5,31 @@ import zlib
 import base45
 import cbor2
 from cose.messages import CoseMessage
+
+from pygments import highlight
+from pygments.lexers import JsonLexer
+from pygments.formatters import TerminalFormatter
+
  
 if len(sys.argv) > 1:
     payload = sys.argv[1][4:]
-    print("\n\033[1m\033[95m Decoding payload: "+ payload + "\033[0m\n")
+    print("\n\033[1m" + "*** Decoding payload: " + "\033[0m" + payload + "\n")
     
     # decode Base45 (remove HC1: prefix)
     decoded = base45.b45decode(payload)
     
     # decompress using zlib
     decompressed = zlib.decompress(decoded)
+
     # decode COSE message (no signature verification done)
     cose = CoseMessage.decode(decompressed)
-    # decode the CBOR encoded payload and print as json
-    print(json.dumps(cbor2.loads(cose.payload), indent=2))
+
+    # decode the CBOR encoded payload as a JSON
+    json_output = json.dumps(cbor2.loads(cose.payload), indent=2)
+
+    # format JSON to be printed
+    formatted_output = highlight(json_output, JsonLexer(), TerminalFormatter())
+    print(formatted_output)
+
 else:
     print("\033[91m (!) Missing payload. Usage: python decode.py 'HC1:6BFNXSI...'")
